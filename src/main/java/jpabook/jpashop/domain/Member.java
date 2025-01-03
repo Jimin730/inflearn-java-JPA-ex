537bdeea4196346941f80f640fe7bf02e227fa99
jimin <dnddnd120@naver.com>
2025-01-03 14:17:54 +0900

    feat: Member 연관관계 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -12,6 +15,10 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    //실무에서는 보통 추가하지 않지만, 예제라 추가
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>(); //관례상 초기 값 arrayList 세팅
 
     public Long getId() {
         return id;
